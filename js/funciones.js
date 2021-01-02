71f35e174cca4016648e72edc6d0e08bb2f63e5d
anhaidcca <anhaidcca@gmail.com>
2021-01-02 13:48:41 -0600

    2ene

--- js/funciones.js
+++ js/funciones.js
@@ -10,3 +10,43 @@ function abrirPanel(panelInicial){
      //Asigna la clase activa al elemento con el id que se recibe como parametro.
      //Esta accion hace visible el panel que corresponde al boton que fue clicado.
      document.getElementById(panelInicial).classList.add("activa")
+}
+
+fetch('../data/bd.json')
+          .then(respuesta=>respuesta.json())
+          .then(datos=>agregarProyectos(datos.proyectos))
+
+ function agregarProyectos(datos){
+          const proyectos = datos
+          const tabla=document.querySelector("#listarProyectos")
+          
+          proyectos.forEach(proyecto=>{
+               tabla.innerHTML+=`
+               
+                    ${proyecto.nombre}
+                    ${proyecto.lenguaje}
+                    ${proyecto.monto}
+               
+               `
+          })
+     }
+     
+     function insertar(evt){
+          evt.preventDefault()
+          
+          const valores=[{
+                nombre:document.getElementById('nombre').value,
+                lenguaje:document.getElementById('lenguaje').value,
+                monto:document.getElementById('monto').value,
+          }]
+     
+          agregarProyectos(valores)
+          limpiarFormulario()
+     }
+     
+     function limpiarFormulario(){
+           // Limpiar datos del formulario luego de presionar el boton Agregar
+           document.getElementById("nombre").value="";
+           document.getElementById("lenguaje").value="";
+           document.getElementById("monto").value="";
+     }
